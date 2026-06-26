6b949bc9b473e26a797ea83731f2362bf7dfe1ad
muhammadahmad <muhammadahmad@DESKTOP-9R880DH>
2026-06-26 10:40:35 +0500

    style: configure global design tokens, fonts, styles, and providers wrapper

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './index.css'
+import App from './App.tsx'
+import { ThemeProvider } from './context/ThemeContext'
+import { CartProvider } from './context/CartContext'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </ThemeProvider>
+  </StrictMode>,
+)
+
